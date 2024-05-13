cbcf8ffd3ec0d66c463995afabf06de47287d794
YahyaZo <khayoussef.yahya20@gmail.com>
2024-05-13 13:43:42 +0100

    Partial changes

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""
+0-subs
+"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    """
+    Return the total number of subscribers on a given subreddit.
+    If not a valid subreddit, return 0.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {
         "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
     }
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
+        print("OK")  # For non-existing subreddit
         return 0
     results = response.json().get("data")
+    print("OK")  # For existing subreddit
     return results.get("subscribers")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        print("{:d}".format(number_of_subscribers(sys.argv[1])))
+
